893ebaea9d4410120a41c31e681f89682efb2a30
Henrique Dezani <dezani@gmail.com>
2021-06-01 18:30:06 -0300

    Versão final da aula 01/06/2021

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat/models/usuario.model.dart';
+import 'package:flutter_chat/models/mensagem.model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +20,20 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final _firestore = FirebaseFirestore.instance;
+  final txtMessageCtrl = TextEditingController();
+
+  Future sendMessage() async {
+    await _firestore.collection('mensagens').add(
+      {
+        'nome': 'Usuário',
+        'mensagem': txtMessageCtrl.text,
+        'data': DateTime.now().millisecondsSinceEpoch,
+        'status': true,
+      },
+    );
+
+    txtMessageCtrl.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,31 +41,77 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter Chat"),
       ),
-      body: StreamBuilder(
-        stream: _firestore.collection('mensagens').snapshots(),
-        builder: (_, snapshot) {
-          if (snapshot.hasError) return Text(snapshot.error.toString());
+      body: Column(
+        children: [
+          ListaMensagens(),
+          Container(
+            margin: const EdgeInsets.fromLTRB(20, 30, 20, 30),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                      controller: txtMessageCtrl,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      maxLines: 5,
+                      minLines: 1),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: sendMessage,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
-          if (!snapshot.hasData)
-            return Center(child: CircularProgressIndicator());
+class ListaMensagens extends StatelessWidget {
+  final _firestore = FirebaseFirestore.instance;
 
-          return ListView.builder(
+  Future delete(String id) async {
+    await _firestore.collection('mensagens').doc(id).delete();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firestore
+          .collection('mensagens')
+          .where('status', isEqualTo: true)
+          .orderBy('data', descending: true)
+          .snapshots(),
+      builder: (_, snapshot) {
+        if (snapshot.hasError) return Text(snapshot.error.toString());
+
+        if (!snapshot.hasData)
+          return Center(child: CircularProgressIndicator());
+
+        return Flexible(
+          child: ListView.builder(
+            reverse: true,
             itemCount: snapshot.data.docs.length,
             itemBuilder: (_, index) {
-              // var doc = snapshot.data.docs[index];
-              var usuario = Usuario.fromMap(snapshot.data.docs[index].data());
-              return ListTile(
-                title: Text(usuario.nome),
-                subtitle: Text(usuario.email),
+              var mensagem = Mensagem.fromMap(snapshot.data.docs[index].data());
+              return Dismissible(
+                key: Key(snapshot.data.docs[index].id),
+                background: Container(color: Colors.red),
+                onDismissed: (_) => delete(snapshot.data.docs[index].id),
+                child: ListTile(
+                  title: Text(mensagem.nome),
+                  subtitle: Text(mensagem.mensagem),
+                  trailing:
+                      Text("${mensagem.data.month}/${mensagem.data.year}"),
+                ),
               );
-              // return ListTile(
-              //   title: Text(doc['nome']),
-              //   subtitle: Text(doc['email']),
-              // );
             },
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/models/mensagem.model.dart
+++ lib/models/mensagem.model.dart
@@ -1,11 +1,14 @@
 class Mensagem {
   String nome;
   String mensagem;
+  DateTime data;
+  bool status;
 
-  Mensagem({this.nome, this.mensagem});
+  Mensagem({this.nome, this.mensagem, this.status = true});
 
   Mensagem.fromMap(Map<String, dynamic> map) {
     this.nome = map['nome'];
-    this.mensagem = map['email'];
+    this.mensagem = map['mensagem'];
+    this.data = DateTime.fromMillisecondsSinceEpoch(map['data']);
   }
 }
